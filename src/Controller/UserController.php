8b0b4404d6904114291869c342c6670c46fe59d3
TRT2NOW <abdelilahtaourirt37@gmail.com>
2025-04-05 21:49:07 +0200

    corregir el userController

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,13 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Entity\UserRepository;
+
 use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\UserRepository;
 
 #[Route('/user')]
 final class UserController extends AbstractController
