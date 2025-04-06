5e881d59c6b502aae9bbc505b0302ec4bfc60bec
Daniel Martínez <danielmartinez2987@gmail.com>
2025-04-06 12:30:01 +0200

    Added pire_db_V2.sql

---
+++ pire_db_V2.sql
@@ -0,0 +1,352 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 06-04-2025 a las 12:24:53
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `pire_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asignatura`
+--
+
+CREATE TABLE `asignatura` (
+  `id` int(11) NOT NULL,
+  `curso_id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `codigo` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `asignatura`
+--
+
+INSERT INTO `asignatura` (`id`, `curso_id`, `nombre`, `codigo`) VALUES
+(1, 1, 'Programación', 'PROG1'),
+(2, 1, 'Bases de Datos', 'BD1'),
+(3, 1, 'Sistemas Informáticos', 'SI1'),
+(4, 2, 'Desarrollo de Interfaces', 'DI2'),
+(5, 2, 'Acceso a Datos', 'AD2'),
+(6, 2, 'Programación Multimedia', 'PM2');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ciclo`
+--
+
+CREATE TABLE `ciclo` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `ciclo`
+--
+
+INSERT INTO `ciclo` (`id`, `nombre`, `descripcion`) VALUES
+(1, 'DAM', 'Desarrollo de Aplicaciones Multiplataforma'),
+(2, 'DAW', 'Desarrollo de Aplicaciones Web'),
+(3, 'ASIR', 'Administración de Sistemas Informáticos en Red');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `curso`
+--
+
+CREATE TABLE `curso` (
+  `id` int(11) NOT NULL,
+  `ciclo_id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `curso`
+--
+
+INSERT INTO `curso` (`id`, `ciclo_id`, `nombre`) VALUES
+(1, 1, 'Primer Curso DAM'),
+(2, 1, 'Segundo Curso DAM'),
+(3, 2, 'Primer Curso DAW'),
+(4, 2, 'Segundo Curso DAW'),
+(5, 3, 'Primer Curso ASIR'),
+(6, 3, 'Segundo Curso ASIR');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `doctrine_migration_versions`
+--
+
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20250402195419', '2025-04-02 21:54:30', 79),
+('DoctrineMigrations\\Version20250402201921', '2025-04-02 22:19:40', 441);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `documento`
+--
+
+CREATE TABLE `documento` (
+  `id` int(11) NOT NULL,
+  `asignatura_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `descripcion` longtext DEFAULT NULL,
+  `ruta_archivo` varchar(255) NOT NULL,
+  `fecha_subida` datetime NOT NULL,
+  `aprobado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `documento`
+--
+
+INSERT INTO `documento` (`id`, `asignatura_id`, `user_id`, `titulo`, `descripcion`, `ruta_archivo`, `fecha_subida`, `aprobado`) VALUES
+(1, 4, 2, 'Apuntes 704', 'Descripción para documento 910', '/uploads/doc_440.pdf', '2024-11-21 23:34:08', 1),
+(2, 6, 2, 'Apuntes 80', 'Descripción para documento 382', '/uploads/doc_671.pdf', '2025-01-02 23:34:08', 1),
+(3, 3, 2, 'Apuntes 630', 'Descripción para documento 41', '/uploads/doc_317.pdf', '2025-01-15 23:34:08', 0),
+(4, 1, 2, 'Apuntes 699', 'Descripción para documento 381', '/uploads/doc_807.pdf', '2025-01-01 23:34:08', 1),
+(5, 2, 2, 'Apuntes 960', 'Descripción para documento 142', '/uploads/doc_832.pdf', '2024-11-29 23:34:08', 1),
+(6, 5, 1, 'Apuntes 838', 'Descripción para documento 339', '/uploads/doc_181.pdf', '2024-11-07 23:34:08', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `messenger_messages`
+--
+
+CREATE TABLE `messenger_messages` (
+  `id` bigint(20) NOT NULL,
+  `body` longtext NOT NULL,
+  `headers` longtext NOT NULL,
+  `queue_name` varchar(190) NOT NULL,
+  `created_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `available_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `delivered_at` datetime DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(180) NOT NULL,
+  `roles` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '(DC2Type:json)' CHECK (json_valid(`roles`)),
+  `password` varchar(255) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `is_verified` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `roles`, `password`, `nombre`, `is_verified`) VALUES
+(1, 'alumno@example.com', '[\"ROLE_USER\"]', '123456', 'Alumno Ejemplo', 0),
+(2, 'profesor@example.com', '[\"ROLE_TEACHER\"]', '$2y$13$abcdefghijk1234567890', 'Profesor Ejemplo', 0),
+(7, 'prueba@item.com', '[\"ROLE_USER\"]', '$2y$13$KcGOzcZAJWXlib7P2XDsUenpWAUl9frZVS3Exk43G0KOxbyy3nbZa', 'Pepe', 1),
+(8, 'pruebaa@item.com', '[]', '$2y$13$ED3dryS5YIoPIlUbgmEavulFuwlYkFKpw/eT7k6vj.F82fu2Blli2', 'Prueba', 0),
+(9, 'alumnopaco@example.com', '[]', '$2y$13$p1wSru2rk6vlsXSWxCfqJuWQxk2G8wdZmxCdd9gWYrRjLyd4H/QvW', 'Paco', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `valoracion`
+--
+
+CREATE TABLE `valoracion` (
+  `id` int(11) NOT NULL,
+  `documento_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `puntuacion` smallint(6) NOT NULL,
+  `comentario` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `valoracion`
+--
+
+INSERT INTO `valoracion` (`id`, `documento_id`, `user_id`, `puntuacion`, `comentario`) VALUES
+(1, 2, 1, 3, 'Buen contenido'),
+(2, 1, 1, 1, 'Buen contenido'),
+(3, 3, 1, 4, 'Buen contenido'),
+(4, 5, 1, 3, 'Buen contenido'),
+(5, 4, 1, 5, 'Buen contenido'),
+(6, 6, 1, 3, 'Buen contenido');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `asignatura`
+--
+ALTER TABLE `asignatura`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_9243D6CE87CB4A1F` (`curso_id`);
+
+--
+-- Indices de la tabla `ciclo`
+--
+ALTER TABLE `ciclo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `curso`
+--
+ALTER TABLE `curso`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_CA3B40ECD8F6DC8` (`ciclo_id`);
+
+--
+-- Indices de la tabla `doctrine_migration_versions`
+--
+ALTER TABLE `doctrine_migration_versions`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Indices de la tabla `documento`
+--
+ALTER TABLE `documento`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_B6B12EC7C5C70C5B` (`asignatura_id`),
+  ADD KEY `IDX_B6B12EC7A76ED395` (`user_id`);
+
+--
+-- Indices de la tabla `messenger_messages`
+--
+ALTER TABLE `messenger_messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_75EA56E0FB7336F0` (`queue_name`),
+  ADD KEY `IDX_75EA56E0E3BD61CE` (`available_at`),
+  ADD KEY `IDX_75EA56E016BA31DB` (`delivered_at`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_8D93D649E7927C74` (`email`);
+
+--
+-- Indices de la tabla `valoracion`
+--
+ALTER TABLE `valoracion`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_6D3DE0F445C0CF75` (`documento_id`),
+  ADD KEY `IDX_6D3DE0F4A76ED395` (`user_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `asignatura`
+--
+ALTER TABLE `asignatura`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `ciclo`
+--
+ALTER TABLE `ciclo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `curso`
+--
+ALTER TABLE `curso`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `documento`
+--
+ALTER TABLE `documento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `messenger_messages`
+--
+ALTER TABLE `messenger_messages`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `valoracion`
+--
+ALTER TABLE `valoracion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `asignatura`
+--
+ALTER TABLE `asignatura`
+  ADD CONSTRAINT `FK_9243D6CE87CB4A1F` FOREIGN KEY (`curso_id`) REFERENCES `curso` (`id`);
+
+--
+-- Filtros para la tabla `curso`
+--
+ALTER TABLE `curso`
+  ADD CONSTRAINT `FK_CA3B40ECD8F6DC8` FOREIGN KEY (`ciclo_id`) REFERENCES `ciclo` (`id`);
+
+--
+-- Filtros para la tabla `documento`
+--
+ALTER TABLE `documento`
+  ADD CONSTRAINT `FK_B6B12EC7A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `FK_B6B12EC7C5C70C5B` FOREIGN KEY (`asignatura_id`) REFERENCES `asignatura` (`id`);
+
+--
+-- Filtros para la tabla `valoracion`
+--
+ALTER TABLE `valoracion`
+  ADD CONSTRAINT `FK_6D3DE0F445C0CF75` FOREIGN KEY (`documento_id`) REFERENCES `documento` (`id`),
+  ADD CONSTRAINT `FK_6D3DE0F4A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
