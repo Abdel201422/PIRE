4709e91072ac89fbb6500c467f1b139a53d61587
Jonatan Moreno Barnés <1109.jjmb@gmail.com>
2025-04-08 21:15:33 +0200

    BBDD pire_db_v3

--- pire_db_V3.sql
+++ pire_db_V3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-04-2025 a las 12:42:02
+-- Tiempo de generación: 08-04-2025 a las 21:13:22
 -- Versión del servidor: 10.4.32-MariaDB-log
 -- Versión de PHP: 8.2.12
 
@@ -35,22 +35,23 @@ CREATE TABLE `documento` (
   `descripcion` longtext DEFAULT NULL,
   `ruta_archivo` varchar(255) NOT NULL,
   `fecha_subida` datetime NOT NULL,
-  `aprobado` tinyint(1) NOT NULL,
-  `archivo` varchar(255) NOT NULL
+  `aprobado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `documento`
 --
 
-INSERT INTO `documento` (`id`, `asignatura_id`, `user_id`, `titulo`, `descripcion`, `ruta_archivo`, `fecha_subida`, `aprobado`, `archivo`) VALUES
-(1, 4, 2, 'Apuntes 704', 'Descripción para documento 910', '/uploads/doc_440.pdf', '2024-11-21 23:34:08', 1, ''),
-(2, 6, 2, 'Apuntes 80', 'Descripción para documento 382', '/uploads/doc_671.pdf', '2025-01-02 23:34:08', 1, ''),
-(3, 3, 2, 'Apuntes 630', 'Descripción para documento 41', '/uploads/doc_317.pdf', '2025-01-15 23:34:08', 0, ''),
-(4, 1, 2, 'Apuntes 699', 'Descripción para documento 381', '/uploads/doc_807.pdf', '2025-01-01 23:34:08', 1, ''),
-(5, 2, 2, 'Apuntes 960', 'Descripción para documento 142', '/uploads/doc_832.pdf', '2024-11-29 23:34:08', 1, ''),
-(6, 5, 1, 'Apuntes 838', 'Descripción para documento 339', '/uploads/doc_181.pdf', '2024-11-07 23:34:08', 1, ''),
-(7, 1, 1, 'Ejemplo', 'Ejemplo', 'Quitar?', '2025-04-08 12:27:00', 1, 'archivos/solucion-actividad-2-67f4fa5365409.pdf');
+INSERT INTO `documento` (`id`, `asignatura_id`, `user_id`, `titulo`, `descripcion`, `ruta_archivo`, `fecha_subida`, `aprobado`) VALUES
+(1, 4, 2, 'Apuntes 704', 'Descripción para documento 910', '/uploads/doc_440.pdf', '2024-11-21 23:34:08', 1),
+(2, 6, 2, 'Apuntes 80', 'Descripción para documento 382', '/uploads/doc_671.pdf', '2025-01-02 23:34:08', 1),
+(3, 3, 2, 'Apuntes 630', 'Descripción para documento 41', '/uploads/doc_317.pdf', '2025-01-15 23:34:08', 0),
+(4, 1, 2, 'Apuntes 699', 'Descripción para documento 381', '/uploads/doc_807.pdf', '2025-01-01 23:34:08', 1),
+(5, 2, 2, 'Apuntes 960', 'Descripción para documento 142', '/uploads/doc_832.pdf', '2024-11-29 23:34:08', 1),
+(6, 5, 1, 'Apuntes 838', 'Descripción para documento 339', '/uploads/doc_181.pdf', '2024-11-07 23:34:08', 1),
+(7, 1, 1, 'Ejemplo', 'Ejemplo', 'Quitar?', '2025-04-08 12:27:00', 1),
+(8, 1, 1, 'EJemplo', 'ejemplo', 'Quitar?', '2025-04-08 12:52:00', 1),
+(9, 1, 1, 'archivo ejemplo', 'Ejemplo', 'archivos/practicaHTTPS-67f573a2435c7.pdf', '2025-04-08 21:05:00', 1);
 
 --
 -- Índices para tablas volcadas
@@ -72,7 +73,7 @@ ALTER TABLE `documento`
 -- AUTO_INCREMENT de la tabla `documento`
 --
 ALTER TABLE `documento`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Restricciones para tablas volcadas
