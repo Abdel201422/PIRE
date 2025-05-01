9d9faea84670d188c50f7b6b4035767ba905a951
Daniel Martínez <danielmartinez2987@gmail.com>
2025-05-01 20:11:35 +0200

    Update Modificaciones en frontend añadiendo componentes y html

---
+++ Frontend/assets/js/dashboard.js
@@ -0,0 +1,5 @@
+// js/dashboard.js
+
+import { infoDashboard } from './api/dashboard.js';
+
+infoDashboard()
--- Frontend/assets/js/includes.js
+++ Frontend/assets/js/includes.js
@@ -2,13 +2,36 @@
 
 // Carga dinámica del componente header
 document.addEventListener('DOMContentLoaded', () => {
-    const headerContainer = document.getElementById('header');
-    if (headerContainer) {
-        fetch('/components/header.html')
+    
+    const headerMainContainer = document.getElementById('header');
+    if (headerMainContainer) {
+        fetch('/components/header_main.html')
             .then(response => response.text())
             .then(html => {
                 headerContainer.innerHTML = html
             })
             .catch(error => console.error('Error al cargar el header:', error))
-        }
+    }
+
+    // Cargar dinámicamente el Header
+    const headerContainer = document.getElementById('header_dashboard');
+    if (headerContainer) {
+        fetch('/components/header_dashboard.html')
+            .then(response => response.text())
+            .then(html => {
+                headerContainer.innerHTML = html;
+            })
+            .catch(error => console.error('Error al cargar el header:', error));
+    }
+
+    // Cargar dinámicamente el Sidebar
+    const sidebarContainer = document.getElementById('sidebar');
+    if (sidebarContainer) {
+        fetch('/components/sidebar.html')
+            .then(response => response.text())
+            .then(html => {
+                sidebarContainer.innerHTML = html;
+            })
+            .catch(error => console.error('Error al cargar el sidebar:', error));
+    }
 })
