bcbe296f19a78c9a864db6ab6e6e512b318c3cbd
jonatanmoba <1109.jjmb@gmail.com>
2025-05-08 11:32:12 +0200

    cambio de iconos en el sidebar, header_dashboard, dashboard

--- Backend/src/Entity/Asignatura.php
+++ Backend/src/Entity/Asignatura.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 use App\Repository\AsignaturaRepository;
 
 #[ORM\Entity(repositoryClass: AsignaturaRepository::class)]
@@ -17,6 +19,14 @@ class Asignatura
     #[ORM\ManyToOne(targetEntity: Curso::class, inversedBy: 'asignaturas')]
     #[ORM\JoinColumn(name: "curso_cod_curso", referencedColumnName: "cod_curso", nullable: false)]
     private ?Curso $curso = null;
+    
+    #[ORM\OneToMany(mappedBy: 'asignatura', targetEntity: Documento::class)]
+    private Collection $documentos;
+    
+    public function __construct()
+    {
+        $this->documentos = new ArrayCollection();
+    }
     
     // Getters y Setters
     public function getNombre(): ?string
@@ -47,5 +57,32 @@ class Asignatura
     public function setCodigo(string $codigo): self 
     { 
         $this->codigo = $codigo; return $this; 
+    }
+    /**
+     * @return Collection<int, Documento>
+     */
+    public function getDocumentos(): Collection
+    {
+        return $this->documentos;
+    }
+
+    public function addDocumento(Documento $documento): self
+    {
+        if (!$this->documentos->contains($documento)) {
+            $this->documentos->add($documento);
+            $documento->setAsignatura($this);
+        }
+        return $this;
+    }
+
+    public function removeDocumento(Documento $documento): self
+    {
+        if ($this->documentos->removeElement($documento)) {
+            // set the owning side to null (unless already changed)
+            if ($documento->getAsignatura() === $this) {
+                $documento->setAsignatura(null);
+            }
+        }
+        return $this;
     }
 }
