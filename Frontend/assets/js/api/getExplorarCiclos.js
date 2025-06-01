a9387aa062bd01bb2f9c071a8cc6c4d4c1178dfb
Daniel Martínez <danielmartinez2987@gmail.com>
2025-06-01 16:37:01 +0200

    Update Frontend

--- Frontend/assets/js/api/getExplorarCiclos.js
+++ Frontend/assets/js/api/getExplorarCiclos.js
@@ -15,6 +15,37 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (!explorarCiclos || !tabsContainer) return
 
+    const leftArrow = document.getElementById('leftArrow')
+    const rightArrow = document.getElementById('rightArrow')
+
+    if (leftArrow && rightArrow && tabsContainer) {
+
+        function updateArrows() {
+            if (tabsContainer.scrollLeft > 0) {
+                leftArrow.classList.remove('hidden')
+            } else {
+                leftArrow.classList.add('hidden')
+            }
+
+            if (tabsContainer.scrollLeft + tabsContainer.clientWidth < tabsContainer.scrollWidth - 1) {
+                rightArrow.classList.remove('hidden')
+            } else {
+                rightArrow.classList.add('hidden')
+            }
+        }
+
+        tabsContainer.addEventListener('scroll', updateArrows)
+        updateArrows()
+
+        leftArrow.addEventListener('click', () => {
+            tabsContainer.scrollBy({ left: -150, behavior: 'smooth' })
+        })
+
+        rightArrow.addEventListener('click', () => {
+            tabsContainer.scrollBy({ left: 150, behavior: 'smooth' })
+        })
+    }
+
     fetch(`${BACKEND_URL}/api/ciclos/completos`, {
         method: 'GET',
         headers: { 'Authorization': `Bearer ${token}` },
@@ -103,39 +134,10 @@ document.addEventListener('DOMContentLoaded', function () {
                 })
             })
 
+            updateArrows()
+
         })
         .catch(error => {
             console.error('Error al cargar los ciclos:', error)
         })
-
-    const leftArrow = document.getElementById('leftArrow')
-    const rightArrow = document.getElementById('rightArrow')
-
-    if (leftArrow && rightArrow && tabsContainer) {
-
-        function updateArrows() {
-            if (tabsContainer.scrollLeft > 0) {
-                leftArrow.classList.remove('hidden')
-            } else {
-                leftArrow.classList.add('hidden')
-            }
-
-            if (tabsContainer.scrollLeft + tabsContainer.clientWidth < tabsContainer.scrollWidth - 1) {
-                rightArrow.classList.remove('hidden')
-            } else {
-                rightArrow.classList.add('hidden')
-            }
-        }
-
-        tabsContainer.addEventListener('scroll', updateArrows)
-        updateArrows()
-
-        leftArrow.addEventListener('click', () => {
-            tabsContainer.scrollBy({ left: -150, behavior: 'smooth' })
-        })
-
-        rightArrow.addEventListener('click', () => {
-            tabsContainer.scrollBy({ left: 150, behavior: 'smooth' })
-        })
-    }
 })
